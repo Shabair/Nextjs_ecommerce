9fcf70de22473e8615efc80905c47bd86c16f5b6
Shabair <shabair143@gmail.com>
2025-11-19 15:08:34 +1000

    fix: frontend tsconfig paths

--- packages/frontend/app/layout.tsx
+++ packages/frontend/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Layout from "@/containers/Layout"
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,8 +28,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      > 
+        <Layout>
+          {children}
+        </Layout>
       </body>
     </html>
   );
